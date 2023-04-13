bafc2e97a025f40c5e1aebc25d256763108e6b1b
devjeffsilvaphb <129746059+devjeffsilvaphb@users.noreply.github.com>
2023-04-13 10:00:36 -0300

    Create learn py3.py

---
+++ learn py3.py
@@ -0,0 +1,24 @@
+#EXAMPLES TO LEARN BASIC COMANDS IN PYTHON//APRENDA COMANDOS BÁSICOS DO PYTHON!!
+
+#PRINT IN SCREEN//ESCREVER NA TELA
+
+#use "print("null")" 
+
+print("Learn print in python!! Now")
+
+#VARIABLES//VARIÁVEIS
+
+#use name = value
+
+python = "Hello, World!"
+name = "PYTHON"
+
+#use a comand print and variable
+
+print(name + " 3")
+print(python)
+
+#use "+" for concatenate word
+
+#End, Thanks for learn or see my examples basic in python3, bay!
+
